36642c96d0971069ef263b3c36a3e6db0f26c2d5
GalinGeorgiev92 <g.georgiev92@abv.bg>
2019-11-03 16:22:15 +0200

    Added Linear Data structures - exercises.

---
+++ Linear-Data-Structures/Custom List/SortWords/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace SortWords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split();
+
+            var result = input.OrderBy(x => x);
+
+            Console.WriteLine($"{string.Join(" ", result)}");
+
+
+        }
+    }
+}
