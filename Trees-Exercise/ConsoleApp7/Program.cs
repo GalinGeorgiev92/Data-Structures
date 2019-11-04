c0c0f37ecab9b3bd85e8f2f0c435a6ea6fca6901
GalinGeorgiev92 <g.georgiev92@abv.bg>
2019-11-04 17:11:39 +0200

    Working with trees.

---
+++ Trees-Exercise/ConsoleApp7/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static Dictionary<int, Tree<int>> tree = new Dictionary<int, Tree<int>>();
+
+        static void Main(string[] args)
+        {
+            int numberOfNodes = int.Parse(Console.ReadLine());
+
+            for(int i = 0; i< numberOfNodes - 1; i++)
+            {
+                int[] nodes = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+                int parent = nodes[0];
+                int child = nodes[1];
+
+                if(!tree.ContainsKey(parent))
+                {
+                    tree.Add(parent, new Tree<int>(parent));
+                }
+
+                if(!tree.ContainsKey(child))
+                {
+                    tree.Add(child, new Tree<int>(child));
+                }
+
+                Tree<int> parentNode = tree[parent];
+                Tree<int> childNode = tree[child];
+
+                parentNode.Children.Add(childNode);
+                childNode.Parent = parentNode;
+            }
+
+            Tree<int> root = tree.FirstOrDefault(x => x.Value.Parent == null).Value;
+
+            int sum = int.Parse(Console.ReadLine());
+
+            List<Tree<int>> leaf = tree.Values.Where(x => x.Children.Count == 0).ToList();
+        }
+
+        class Tree<T>
+        {
+            public T Value { get; set; }
+
+            public List<Tree<T>> Children { get; set; }
+
+            public Tree<T> Parent { get; set; }
+
+            public Tree(T value, params Tree<T>[] children)
+            {
+                this.Value = value;
+                this.Children = new List<Tree<T>>(children);
+            }
+        }
+    }
+}
