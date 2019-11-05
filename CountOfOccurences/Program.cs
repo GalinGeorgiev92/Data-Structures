e0e4d6948183da36a3c5c3c4f6904495a4f25bbd
GalinGeorgiev92 <g.georgiev92@abv.bg>
2019-11-05 13:27:29 +0200

    Refactored some parts of exercises.

---
+++ CountOfOccurences/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CountOfOccurences
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            var result = new Dictionary<int, int>();
+
+            for(int i =0;i < input.Length; i++)
+            {
+                if(!result.ContainsKey(input[i]))
+                {
+                    result[input[i]] = 0;
+                }
+
+                result[input[i]]++;
+            }
+
+            foreach (var kvp in result)
+            {
+                Console.WriteLine($"{kvp.Key} -> {kvp.Value} times");
+            }
+        }
+    }
+}
---
+++ SumAndAverage/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace SumAndAverage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            var sum = input.Sum();
+            var average = input.Average();
+
+            Console.Write($"Sum={sum}; ");
+            Console.WriteLine($"Average={average:f2}");
+        }
+    }
+}
