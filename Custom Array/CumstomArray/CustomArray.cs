bea5956e2fe7c34a9149c411e602295b0727ee9a
GalinGeorgiev92 <g.georgiev92@abv.bg>
2019-11-05 14:07:17 +0200

    Added custom array.

---
+++ Custom Array/CumstomArray/CustomArray.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CumstomArray
+{
+    public class CustomArray<T>
+    {
+        private T[] data;
+
+        public CustomArray()
+        {
+            this.data = new T[4];
+            this.Length = 4;
+        }
+
+        public int Length { get; private set; }
+
+        public T this[int index]
+        {
+            get
+            {
+                if (index >= this.Length || index < 0)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+
+                return this.data[index];
+            }
+
+            set
+            {
+                if (index >= this.Length || index < 0)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+
+                this.data[index] = value;
+            }
+        }
+
+        public void Add(int position, T item)
+        {
+            if (position < 0 || position >= this.data.Length)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            this.data[position] = item;
+        }
+
+        public string ToString()
+        {
+            var result = "";
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                result += this.data[i];
+            }
+
+            return result;
+        }
+
+        public string GetType()
+        {
+            var result = this.data.GetType();
+            return result.ToString();
+        }
+    }
+}
---
+++ Custom Array/CumstomArray/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CumstomArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomArray<int> custom = new CustomArray<int>();
+
+            custom.Add(0, 1);
+            custom.Add(1, 5);
+
+
+            for (int i = 0; i < custom.Length; i++)
+            {
+                Console.WriteLine(custom[i]);
+            }
+
+            Console.WriteLine(custom.GetType());
+
+            custom.Add(-1, 5);
+
+            custom.Add(5, 7);
+
+            CustomArray<string> customString = new CustomArray<string>();
+
+            customString.Add(0, "Something");
+
+            Console.WriteLine(customString[0]);
+        }
+    }
+}
