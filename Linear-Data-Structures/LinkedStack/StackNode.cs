53bde9333103bf42e96f2db2d080e32fb07a3047
GalinGeorgiev92 <g.georgiev92@abv.bg>
2019-11-20 20:55:36 +0200

    Refactored Heaps.

---
+++ Linear-Data-Structures/LinkedStack/StackNode.cs
@@ -0,0 +1,18 @@
+﻿namespace LinkedStack
+{
+    public partial class LinkedStack<T>
+    {
+        private class StackNode
+        {
+            public T Value { get; set; }
+
+            public StackNode Next { get; set; }
+
+            public StackNode(T value, StackNode next)
+            {
+                this.Value = value;
+                this.Next = next;
+            }
+        }
+    }
+}
