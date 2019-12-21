f854db7f06a24591db9c4f671c946e5be6d7ffd1
GalinGeorgiev92 <g.georgiev92@abv.bg>
2019-12-21 13:43:24 +0200

    Added some exercises with stacks and que.

---
+++ Linear Data Structures - Stacks and Ques/CalculateSequenceWithAQueue/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CalculateSequenceWithAQueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = int.Parse(Console.ReadLine());
+
+            Queue queue = new Queue();
+            queue.Enqueue(input);
+            int index = 0;
+            int[] members = new int[50];
+
+            while(true)
+            {
+                int element = (int)queue.Dequeue();
+
+                members[index++] = element;
+
+                if(index == 50)
+                {
+                    break;
+                }
+
+                queue.Enqueue(element + 1);
+                queue.Enqueue(2 * element + 1);
+                queue.Enqueue(element + 2);
+            }
+
+            while(queue.Count > 0)
+            {
+                Console.Write($"{queue.Dequeue()}, ");
+            }
+        }
+    }
+}
---
+++ Linear Data Structures - Stacks and Ques/ReverseNumbersWithAStack/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ReverseNumbersWithAStack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            var stack = new Stack(input);
+
+            while(stack.Count != 0)
+            {
+                Console.Write(stack.Pop() + " ");
+            }
+        }
+    }
+}
